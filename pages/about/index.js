9e62384ec621e8d0b805f2dd4b0e37a8ad591e70
Zach Bailey <zbailey83@users.noreply.github.com>
2023-09-09 19:35:15 -0600

    On branch main new file:   .eslintrc.json new file:   .gitignore new file:   components/Avatar.js new file:   components/Bulb.js new file:   components/Circles.js new file:   components/Header.js new file:   components/Layout.js new file:   components/Nav.js new file:   components/ParticlesContainer.js new file:   components/ProjectsBtn.js new file:   components/ServiceSlider.js new file:   components/Socials.js new file:   components/TestimonialSlider.js new file:   components/TopLeftImg.js new file:   components/Transition.js new file:   components/WorkSlider.js new file:   next.config.js new file:   package-lock.json new file:   package.json new file:   pages/_app.js new file:   pages/about/index.js new file:   pages/api/hello.js new file:   pages/contact/index.js new file:   pages/index.js new file:   pages/services/index.js new file:   pages/testimonials/index.js new file:   pages/work/index.js new file:   postcss.config.js new file:   public/avatar-zbhd.png new file:   public/avatar.png new file:   public/bg-explosion.png new file:   public/bulb.png new file:   public/circle-star.svg new file:   public/circles.png new file:   public/logo-zb.svg new file:   public/logo-zc.svg new file:   public/logo.svg new file:   public/logoz.svg new file:   public/logozc.svg new file:   public/rounded-text.png new file:   public/site-bg.svg new file:   public/t-avt-1.png new file:   public/t-avt-2.png new file:   public/t-avt-3.png new file:   public/thumb1.jpg new file:   public/thumb2.jpg new file:   public/thumb3.jpg new file:   public/thumb4.jpg new file:   public/top-left-img.png new file:   styles/globals.css new file:   tailwind.config.js new file:   variants.js

---
+++ pages/about/index.js
@@ -0,0 +1,207 @@
+import React, { useState } from 'react';
+
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from 'react-icons/fa';
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from 'react-icons/si';
+
+// about data
+export const aboutData = [
+  {
+    title: 'skills',
+    info: [
+      {
+        title: 'Web Development',
+        icons: [
+          <FaHtml5 key="FaHtml5" />,
+          <FaCss3 key="FaCss3" />,
+          <FaJs key="FaJs" />,
+          <FaReact key="FaReact" />,
+          <SiNextdotjs key="SiNextdotjs" />,
+          <SiFramer key="SiFramer" />,
+          <FaWordpress key="FaWordpress" />,
+        ],
+      },
+      {
+        title: 'UI/UX Design',
+        icons: [
+          <FaFigma key="FaFigma" />,
+          <SiAdobexd key="SiAdobexd" />,
+          <SiAdobephotoshop key="SiAdobephotoshop" />,
+        ],
+      },
+    ],
+  },
+  {
+    title: 'awards',
+    info: [
+      {
+        title: 'Webby Awards - Honoree',
+        stage: '2011 - 2012',
+      },
+      {
+        title: 'Adobe Design Achievement Awards - Finalist',
+        stage: '2009 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'experience',
+    info: [
+      {
+        title: 'UX/UI Designer - XYZ Company',
+        stage: '2012 - 2023',
+      },
+      {
+        title: 'Web Developer - ABC Agency',
+        stage: '2010 - 2012',
+      },
+      {
+        title: 'Intern - DEF Corporation',
+        stage: '2008 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'credentials',
+    info: [
+      {
+        title: 'Web Development - ABC University, LA, CA',
+        stage: '2011',
+      },
+      {
+        title: 'Computer Science Diploma - AV Technical Institute',
+        stage: '2009',
+      },
+      {
+        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+        stage: '2006',
+      },
+    ],
+  },
+];
+
+// components
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+// framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
+// counter
+import CountUp from 'react-countup';
+
+const About = () => {
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+      <Circles />
+      {/* avatar img */}
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='hidden xl:flex absolute bottom-0 -left-[370px]'
+      >
+        <Avatar />
+      </motion.div>
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <motion.h2
+            variants={fadeIn('right', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2'
+          >
+            Let us show you what <span className='text-accent'>PixelPix</span> can do for you!
+          </motion.h2>
+          <motion.p
+            variants={fadeIn('right', 0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'
+          >
+            Staying on top of all the latest marketing and design trends can be a fulltime job in itself. 
+            But what if your designs are setting the trends? 
+          </motion.p>
+          {/* counters */}
+          <motion.div
+            variants={fadeIn('right', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'
+          >
+            <div className='flex flex-1 xl:gap-x-6'>
+              {/* experience */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={5} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                  Years of experience
+                </div>
+              </div>
+              {/* clients */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={70} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                  LinkedIn Learning Certificate Courses Completed
+                </div>
+              </div>
+              {/* projects */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={50} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                  Finished projects
+                </div>
+              </div>
+              {/* awards */}
+              <div className='relative flex-1'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={999} duration={5} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                  Projects To Be Built!
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+        {/* info */}
+        <motion.div
+          variants={fadeIn('left', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='flex flex-col w-full xl:max-w-[48%] h-[480px]'
+        >
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
