49973b5e8c614820d37704cdaf0ca3ecbc21e7d5
zer0cool <zbailey83@users.noreply.github.com>
2023-09-14 04:59:54 -0600

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -5,19 +5,19 @@ export const workSlider = {
       images: [
         {
           title: 'title',
-          path: '/thumb1.jpg',
+          path: '/ai-thumb.png',
         },
         {
           title: 'title',
-          path: '/thumb2.jpg',
+          path: '/radio-thumb.png',
         },
         {
           title: 'title',
-          path: '/thumb3.jpg',
+          path: '/prompts25-thumb.png',
         },
         {
           title: 'title',
-          path: '/thumb4.jpg',
+          path: '/castle-thumb.png',
         },
       ],
     },
@@ -25,19 +25,19 @@ export const workSlider = {
       images: [
         {
           title: 'title',
-          path: '/thumb4.jpg',
+          path: '/pixelpixghost-thumb.png',
         },
         {
           title: 'title',
-          path: '/thumb1.jpg',
+          path: '/doom2-thumb.png',
         },
         {
           title: 'title',
-          path: '/thumb2.jpg',
+          path: '/doom2-thumb.png',
         },
         {
           title: 'title',
-          path: '/thumb3.jpg',
+          path: '/pixelpixghost-thumb.png',
         },
       ],
     },
@@ -84,15 +84,15 @@ const WorkSlider = () => {
                       {/* image */}
                       <Image src={image.path} width={500} height={300} alt='' />
                       {/* overlay gradient */}
-                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-75 transition-all duration-700'></div>
                       {/* title */}
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                           {/* title part 1 */}
-                          <div className='delay-100'>LIVE</div>
+                          <div className='delay-100'>PromptPro</div>
                           {/* title part 2 */}
                           <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
-                            PROJECT
+                            <a href="https://www.aiguy.us">AI News & Tools</a>
                           </div>
                           {/* icon */}
                           <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
