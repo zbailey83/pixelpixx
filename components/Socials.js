989887d55a7ebcee924a38111f22a4ce3b0a0bd5
zer0cool <zbailey83@users.noreply.github.com>
2023-09-24 17:45:59 -0600

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -17,7 +17,7 @@ const Socials = () => {
       <Link href={'https://twitter.com/zbailey83'} className='hover:text-accent transition-all duration-300'>
         <RiTwitterLine />
       </Link>
-      <Link href={'https://linkedin.com/zbailey83'} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/zbailey83'} className='hover:text-accent transition-all duration-300'>
         <RiLinkedinLine />
       </Link>
       <Link href={'https://promptpro.medium.com'} className='hover:text-accent transition-all duration-300'>
