d95a71acf2c49b19d6f2a6cbe4fa114e8bbae12f
zer0cool <zbailey83@users.noreply.github.com>
2023-09-24 19:42:27 -0600

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -81,10 +81,10 @@ const WorkSlider = () => {
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                           {/* title part 1 */}
-                          <div className='delay-100'>PromptPro</div>
+                          <div className='delay-100'>Previous</div>
                           {/* title part 2 */}
                           <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
-                            <a href="https://www.aiguy.us">AI News & Tools</a>
+                            <a href="https://www.aiguy.us">Work</a>
                           </div>
                           {/* icon */}
                           <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
