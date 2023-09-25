a4da67e635a8ce5b02a06e6e0f51fd8f1014fe22
zer0cool <zbailey83@users.noreply.github.com>
2023-09-24 19:47:03 -0600

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -11,6 +11,8 @@ import {
   RiPinterestLine,
 } from 'react-icons/ri';
 
+import { SiGumroad } from "react-icons/si";
+
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
@@ -19,6 +21,9 @@ const Socials = () => {
       </Link>
       <Link href={'https://www.linkedin.com/in/zbailey83'} className='hover:text-accent transition-all duration-300'>
         <RiLinkedinLine />
+      </Link>
+     <Link href={'https://promptpro.gumroad.com'} className='hover:text-accent transition-all duration-300'>
+        <SiGumroad />
       </Link>
       <Link href={'https://promptpro.medium.com'} className='hover:text-accent transition-all duration-300'>
         <RiMediumLine />
