e6a38e31c4c671d94b4193437c12c72aa1b6b39d
zer0cool <zbailey83@users.noreply.github.com>
2023-09-24 20:02:17 -0600

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -82,7 +82,7 @@ const WorkSlider = () => {
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
                         <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                           {/* title part 1 */}
-                          <div className='delay-100'><SiGnometerminal /> Previous</div>
+                          <div className='delay-100'>Previous</div>
                           {/* title part 2 */}
                           <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
                             <a href="https://www.aiguy.us">Work</a>
